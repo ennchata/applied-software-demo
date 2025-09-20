5c353be9ba3fa67eccd2e67801a09441c5fddad4
viskag <s144510@ap.be>
2025-09-21 01:44:03 +0200

    unit testing for update

--- AP.DemoProject.UnitTests/Test1.cs
+++ AP.DemoProject.UnitTests/Test1.cs
@@ -1,8 +1,198 @@
-﻿namespace AP.DemoProject.UnitTests {
+﻿using AP.DemoProject.Application.CQRS.Cities;
+using FluentValidation.TestHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Threading.Tasks;
+using AP.DemoProject.Application.Interfaces;
+
+namespace AP.DemoProject.Application.UnitTests.CQRS.Cities
+{
     [TestClass]
-    public sealed class Test1 {
-        [TestMethod]
-        public void TestMethod1() {
+    public class UpdateCityValidatorTests
+    {
+        private readonly Mock<IUnitOfWork> _mockUow;
+        private readonly UpdateCityValidator _validator;
+
+        public UpdateCityValidatorTests()
+        {
+            _mockUow = new Mock<IUnitOfWork>();
+            _validator = new UpdateCityValidator(_mockUow.Object);
         }
+
+        [TestMethod]
+        public async Task TestPopulationNegative()
+        {
+            // Arrange
+            var command = new UpdateCityCommand
+            {
+                Population = -1,
+                CountryId = 1
+            };
+
+            // Act
+            var result = await _validator.TestValidateAsync(command);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Population)
+                .WithErrorMessage("inwoners aantal moet positief getal zijn");
+        }
+
+        [TestMethod]
+        public async Task TestPopulationVeryNegative()
+        {
+            // Arrange
+            var command = new UpdateCityCommand
+            {
+                Population = -100,
+                CountryId = 1
+            };
+
+            // Act
+            var result = await _validator.TestValidateAsync(command);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Population)
+                .WithErrorMessage("inwoners aantal moet positief getal zijn");
+        }
+
+        [TestMethod]
+        public async Task TestPopulationExceedingTenMillion()
+        {
+            // Arrange
+            var command = new UpdateCityCommand
+            {
+                Population = 10000001,
+                CountryId = 1
+            };
+
+            // Act
+            var result = await _validator.TestValidateAsync(command);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Population)
+                .WithErrorMessage("inwoners aantal mag niet meer dan 10mil zijn");
+        }
+
+        [TestMethod]
+        public async Task TestPopulationExceedingTwentyMillion()
+        {
+            // Arrange
+            var command = new UpdateCityCommand
+            {
+                Population = 20000000,
+                CountryId = 1
+            };
+
+            // Act
+            var result = await _validator.TestValidateAsync(command);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Population)
+                .WithErrorMessage("inwoners aantal mag niet meer dan 10mil zijn");
+        }
+
+        [TestMethod]
+        public async Task TestPopulationZero()
+        {
+            // Arrange
+            var command = new UpdateCityCommand
+            {
+                Population = 0,
+                CountryId = 1
+            };
+
+            // Act
+            var result = await _validator.TestValidateAsync(command);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.Population);
+        }
+
+        [TestMethod]
+        public async Task TestPopulationWithinLimit()
+        {
+            // Arrange
+            var command = new UpdateCityCommand
+            {
+                Population = 5000000,
+                CountryId = 1
+            };
+
+            // Act
+            var result = await _validator.TestValidateAsync(command);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.Population);
+        }
+
+        [TestMethod]
+        public async Task TestPopulationAtLimit()
+        {
+            // Arrange
+            var command = new UpdateCityCommand
+            {
+                Population = 10000000,
+                CountryId = 1
+            };
+
+            // Act
+            var result = await _validator.TestValidateAsync(command);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.Population);
+        }
+
+        [TestMethod]
+        public async Task TestCountryIdLessThanMinimum()
+        {
+            // Arrange
+            var command = new UpdateCityCommand
+            {
+                Population = 10000,
+                CountryId = -5
+            };
+
+            // Act
+            var result = await _validator.TestValidateAsync(command);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.CountryId)
+                .WithErrorMessage("kies een land aub");
+        }
+
+        [TestMethod]
+        public async Task TestCountryIdWithinLimits()
+        {
+            // Arrange
+            var command = new UpdateCityCommand
+            {
+                Population = 10000,
+                CountryId = -3  // Greater than -4
+            };
+
+            // Act
+            var result = await _validator.TestValidateAsync(command);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.CountryId);
+        }
+
+        [TestMethod]
+        public async Task TestCountryIdPositive()
+        {
+            // Arrange
+            var command = new UpdateCityCommand
+            {
+                Population = 10000,
+                CountryId = 1
+            };
+
+            // Act
+            var result = await _validator.TestValidateAsync(command);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.CountryId);
+        }
+
     }
 }
