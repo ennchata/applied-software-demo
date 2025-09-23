1665e543a1e3e1cb355faf17ef2f6963a3e38063
Langbeen Matti (s154142) <47920276+matti-langbeen@users.noreply.github.com>
2025-09-23 13:54:13 +0200

    Remove other project reference init methods from WebApp

--- AP.DemoProject.WebApp/Program.cs
+++ AP.DemoProject.WebApp/Program.cs
@@ -1,10 +1,4 @@
-using AP.DemoProject.Application.Extensions;
-using AP.DemoProject.Application.Interfaces;
-using AP.DemoProject.Infrastructure.Contexts;
-using AP.DemoProject.Infrastructure.Extensions;
-using AP.DemoProject.Infrastructure.Services;
 using AP.DemoProject.WebApp.Components;
-using Microsoft.AspNetCore.Components;
 
 namespace AP.DemoProject.WebApp {
     public class Program {
@@ -12,8 +6,6 @@ namespace AP.DemoProject.WebApp {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.RegisterApplication();
-            builder.Services.RegisterInfrastructure();
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
             builder.Services.AddControllers();
