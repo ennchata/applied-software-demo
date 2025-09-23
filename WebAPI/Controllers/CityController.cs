e6b88d6ba210fc9fa256693e149188c458ac4647
Langbeen Matti (s154142) <47920276+matti-langbeen@users.noreply.github.com>
2025-09-23 13:54:54 +0200

    Add HTTP Post for City

--- WebAPI/Controllers/CityController.cs
+++ WebAPI/Controllers/CityController.cs
@@ -3,22 +3,33 @@ using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AP.DemoProject.WebApp.Controllers {
+namespace AP.DemoProject.WebApp.Controllers
+{
     [Route("api/[controller]")]
     [ApiController]
-    public class CityController : ControllerBase {
+    public class CityController : ControllerBase
+    {
         private readonly IMediator _mediator;
 
-        public CityController(IMediator mediator) {
+        public CityController(IMediator mediator)
+        {
             _mediator = mediator;
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll([FromQuery] int? pageNumber, [FromQuery] int? pageSize) {
-            return Ok(await _mediator.Send(new GetAllCitiesQuery() {
+        public async Task<IActionResult> GetAll([FromQuery] int? pageNumber, [FromQuery] int? pageSize)
+        {
+            return Ok(await _mediator.Send(new GetAllCitiesQuery()
+            {
                 PageNumber = pageNumber ?? 1,
                 PageSize = pageSize ?? 20
             }));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CityDTO dto)
+        {
+            return Ok(await _mediator.Send(new AddCityCommand() { City = dto }));
         }
     }
 }
