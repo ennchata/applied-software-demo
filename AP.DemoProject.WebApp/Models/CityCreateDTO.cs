fefbf84d83ccdf678ff6bad4478edf4f09ed4e58
matti-langbeen <47920276+matti-langbeen@users.noreply.github.com>
2025-09-26 16:49:27 +0200

    Delete WebApp

--- AP.DemoProject.WebApp/Models/CityCreateDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace AP.DemoProject.WebApp.Models
-{
-    public class CityCreateDTO
-    {
-        public string Name { get; set; } = string.Empty;
-        public int Population { get; set; }
-        public int CountryId { get; set; }
-    }
-}
--- AP.DemoProject.WebApp/Models/CountryDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace AP.DemoProject.WebApp.Models
-{
-    public class CountryDTO
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
